d0220b4c184c90ac77d6df46279468a4f3312545
Gabriel Carvalho Fraga <gcarvalhofraga@gmail.com>
2022-03-02 15:57:04 -0300

    Added the function to register subjects || Adicionei a função para registrar disciplinas

--- escola.c
+++ escola.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#define TAM_ALUNOS 50
-#define TAM_PROFESSORES 50
-#define TAM_DISCIPLINAS 50
+#define TAM_ALUNOS 1
+#define TAM_PROFESSORES 1
+#define TAM_DISCIPLINAS 1
 
 typedef struct
 {
@@ -22,88 +22,128 @@ typedef struct
 
 typedef struct
 {
-    char professor[100];
-    char codigo[6];
+    char codigo[7];
     char nome[100];
+    char professor[100];
     int semestre;
     Pessoa alunos[100];
 
 } Disciplina;
 
-void apagarCadastro(){};
+void apagarCadastro() {}
 
-void atualizarCadastro(){};
+void atualizarCadastro() {}
 
 void cadastroPessoas()
 {
     Pessoa alunos[TAM_ALUNOS];
     Pessoa professores[TAM_PROFESSORES];
 
-    int option, i, j;
+    int i, j;
     char continuar;
     int length = sizeof(alunos) / sizeof(alunos[0]);
 
-    printf("O que deseja cadastrar? \n\t1 - Alunos \n\t2 - Professores\n");
-
-    printf(">> ");
-    scanf("%d", &option);
-    getchar();
-
-    if (option == 1)
+    for (i = 0; i < TAM_ALUNOS; i++)
     {
-        for (i = 0; i < 1; i++)
-        {
-            printf("Nome do aluno: ");
-            fgets(alunos[i].nome, 31, stdin);
-            size_t ln = strlen(alunos[i].nome) - 1; //size_t = unsigned integer type
-            if (alunos[i].nome[ln] == '\n')
-                alunos[i].nome[ln] = '\0'; // caracter de final de string
-            fflush(stdin);
+        printf("Nome do aluno: ");
+        fgets(alunos[i].nome, 31, stdin);
+        size_t ln = strlen(alunos[i].nome) - 1;
+        if (alunos[i].nome[ln] == '\n')
+            alunos[i].nome[ln] = '\0';
+        fflush(stdin);
 
-            printf("Digite o CPF: ");
-            fgets(alunos[i].cpf, 14, stdin);
-            ln = strlen(alunos[i].cpf) - 1;
-            if (alunos[i].cpf[ln] == '\n')
-                alunos[i].cpf[ln] = '\0';
-            fflush(stdin);
+        printf("Digite o CPF: ");
+        fgets(alunos[i].cpf, 14, stdin);
+        ln = strlen(alunos[i].cpf) - 1;
+        if (alunos[i].cpf[ln] == '\n')
+            alunos[i].cpf[ln] = '\0';
+        fflush(stdin);
 
-            printf("Matrícula do aluno: ");
-            fgets(alunos[i].matricula, 14, stdin);
-            ln = strlen(alunos[i].matricula) - 1;
-            if (alunos[i].matricula[ln] == '\n')
-                alunos[i].matricula[ln] = '\0';
-            fflush(stdin);
+        printf("Matrícula do aluno: ");
+        fgets(alunos[i].matricula, 14, stdin);
+        ln = strlen(alunos[i].matricula) - 1;
+        if (alunos[i].matricula[ln] == '\n')
+            alunos[i].matricula[ln] = '\0';
+        fflush(stdin);
 
-            printf("Digite o sexo: ");
-            scanf("%c", &alunos[i].sexo);
-            fflush(stdin);
+        printf("Digite o sexo: ");
+        scanf("%c", &alunos[i].sexo);
+        fflush(stdin);
 
-            printf("Data de nascimento\n");
+        printf("Data de nascimento\n");
 
-            printf("Dia: ");
-            scanf("%d", &alunos[i].dataNascimento.dia);
-            fflush(stdin);
+        printf("Dia: ");
+        scanf("%d", &alunos[i].dataNascimento.dia);
+        fflush(stdin);
 
-            printf("Mês: ");
-            scanf("%d", &alunos[i].dataNascimento.mes);
-            fflush(stdin);
+        printf("Mês: ");
+        scanf("%d", &alunos[i].dataNascimento.mes);
+        fflush(stdin);
 
-            printf("Ano: ");
-            scanf("%d", &alunos[i].dataNascimento.ano);
-            fflush(stdin);
+        printf("Ano: ");
+        scanf("%d", &alunos[i].dataNascimento.ano);
+        fflush(stdin);
 
-            puts(alunos[i].nome);
-            puts(alunos[i].matricula);
-            puts(alunos[i].cpf);
-            printf("%c \n", alunos[i].sexo);
-            printf("%02d / %02d / %d", alunos[i].dataNascimento.dia, alunos[i].dataNascimento.mes, alunos[i].dataNascimento.ano);
-            // printf("Deseja cadastrar outro aluno(a)?\n");
-            // scanf("%c", &continuar);
-        }
+        puts(alunos[i].nome);
+        puts(alunos[i].matricula);
+        puts(alunos[i].cpf);
+        printf("%c \n", alunos[i].sexo);
+        printf("%02d / %02d / %d", alunos[i].dataNascimento.dia, alunos[i].dataNascimento.mes, alunos[i].dataNascimento.ano);
+        // printf("Deseja cadastrar outro aluno(a)?\n");
+        // scanf("%c", &continuar);
     }
 };
 
+void cadastroDisciplina()
+{
+    Disciplina disciplinas[TAM_DISCIPLINAS];
+    int i;
+
+    for (i = 0; i < TAM_DISCIPLINAS; i++)
+    {
+        printf("Informe o código da disciplina: ");
+        fgets(disciplinas[i].codigo, 7, stdin);
+        size_t ln = strlen(disciplinas[i].codigo) - 1;
+        if (disciplinas[i].codigo[ln] == '\n')
+            disciplinas[i].codigo[ln] = '\0';
+        fflush(stdin);
+
+        printf("Informe o nome da disciplina: ");
+        fgets(disciplinas[i].nome, 31, stdin);
+        ln = strlen(disciplinas[i].nome) - 1;
+        if (disciplinas[i].nome[ln] == '\n')
+            disciplinas[i].nome[ln] = '\0';
+        fflush(stdin);
+
+        printf("Informe o nome do professor da disciplina: ");
+        fgets(disciplinas[i].professor, 31, stdin);
+        ln = strlen(disciplinas[i].professor) - 1;
+        if (disciplinas[i].professor[ln] == '\n')
+            disciplinas[i].professor[ln] = '\0';
+        fflush(stdin);
+
+        printf("Informe o semestre da disciplina: ");
+        scanf("%d", &disciplinas[i].semestre);
+
+        printf("%s || ", disciplinas[i].codigo);
+        printf("%s || ", disciplinas[i].nome);
+        printf("%s || ", disciplinas[i].professor);
+        printf("%d", disciplinas[i].semestre);
+    }
+}
+
 void main()
 {
-    cadastroPessoas();
+    // Pessoa alunos[TAM_ALUNOS], professores[TAM_PROFESSORES];
+    // int option;
+
+    // printf("O que deseja cadastrar? \n\t1 - Alunos \n\t2 - Professores\n");
+
+    // printf(">> ");
+    // scanf("%d", &option);
+    // getchar();
+
+    // cadastroPessoas();
+
+    cadastroDisciplina();
 }
