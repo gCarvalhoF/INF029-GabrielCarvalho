36386ddea1ebabc99ed5d89baeb5fa4dee19860e
Gabriel Carvalho Fraga <gcarvalhofraga@gmail.com>
2022-04-08 14:33:32 -0300

    Questão 1 funcionando, mas com problema no ano

--- GabrielCarvalho-20212160051-T1/GabrielCarvalho-20212160051-T1.c
+++ GabrielCarvalho-20212160051-T1/GabrielCarvalho-20212160051-T1.c
@@ -1,26 +1,96 @@
 #include <stdio.h>
 
-validarData(char data[]){
-  int strlen = sinzeof(data) / sizeof(char);
-  int i;
-  int dia, mes, ano;
-  
-  for(i=0; i < strlen; i++){
-    if (strlen == 10){
-      
-    }
-      
-  }
+int convStrInt(char charValue)
+{
+  int intValue;
+
+  if (charValue == '0')
+    intValue = 0;
+  else if (charValue == '1')
+    intValue = 1;
+  else if (charValue == '2')
+    intValue = 2;
+  else if (charValue == '3')
+    intValue = 3;
+  else if (charValue == '4')
+    intValue = 4;
+  else if (charValue == '5')
+    intValue = 5;
+  else if (charValue == '6')
+    intValue = 6;
+  else if (charValue == '7')
+    intValue = 7;
+  else if (charValue == '8')
+    intValue = 8;
+  else if (charValue == '9')
+    intValue = 9;
+
+  return intValue;
 }
 
-void main(){
-  char dataStr;
-  int dataValida;
-  int dia, mes, ano;
+void validarData(char data[], int dataValida[])
+{
+  int i, barraCount = 0;
+  int dia = 0, mes = 0, ano = 0;
 
-  printf("Informe a data (Formatos: dd/mm/aaaa d/m/aa d/m/aaaa)");
-  fgets(dataStr, stdin, 10);
+  for (i = 0; i < 10; i++)
+  {
+    if (data[i] == '/')
+    {
+      barraCount++;
+      continue;
+    }
 
-  dataValida = validarData(dataStr); 
-  
+    if (barraCount == 0 && data[i] != '/' && data[i + 1] != '/')
+    {
+      printf("Dia: %c - %c \n", data[i], data[i + 1]);
+      dia = convStrInt(data[i]) * 10 + convStrInt(data[i + 1]);
+    }
+    else if (dia == 0)
+    {
+      dia = convStrInt(data[i]);
+    }
+
+    if (barraCount == 1 && data[i] != '/' && data[i + 1] != '/')
+    {
+      printf("Mes: %c - %c \n", data[i], data[i + 1]);
+      mes = convStrInt(data[i]) * 10 + convStrInt(data[i + 1]);
+    }
+    else if (mes == 0)
+    {
+      mes = convStrInt(data[i]);
+    }
+
+    if (barraCount == 2 && data[i] != '/' && data[i + 3] != '\0')
+    {
+      printf("Ano: %c - %c - %c - %c \n", data[i], data[i + 1], data[i + 2], data[i + 3]);
+      ano = convStrInt(data[i]) * 1000 + convStrInt(data[i + 1]) * 100 + convStrInt(data[i + 2]) * 10 + convStrInt(data[i + 3]);
+    }
+    else if (ano == 0)
+    {
+      ano = convStrInt(data[i]) * 10 + convStrInt(data[i + 1]);
+    }
+  }
+
+  dataValida[0] = dia;
+  dataValida[1] = mes;
+  dataValida[2] = ano;
+}
+
+void main()
+{
+  char dataStr[11];
+  int dataValida[3];
+
+  printf("Informe a data (Formatos: dd/mm/aaaa d/m/aa d/m/aaaa) ");
+  fgets(dataStr, 11, stdin);
+  int ln = sizeof(dataStr) / sizeof(char) - 1;
+  if (dataStr[ln] == '\n')
+    dataStr[ln] = '\0';
+
+  printf("%d", ln);
+
+  validarData(dataStr, dataValida);
+
+  printf("%02d / %02d / %4d", dataValida[0], dataValida[1], dataValida[2]);
 }
